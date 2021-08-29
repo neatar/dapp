267a1e14b2b86e349b596a8dc59bf2920bbe5210
Ilyar <761285+ilyar@users.noreply.github.com>
2021-08-29 16:59:06 +0300

    use IPFS CID as tokenId

--- Makefile
+++ Makefile
@@ -57,7 +57,7 @@ out/main.wasm:
 build:\
 out/main.wasm
 rebuild: clean-out build
-redeploy: deploy-delete deploy-new
+redeploy: deploy-delete deploy-new call_avatar_create_for_beta_tester
 
 deploy: rebuild
 	near dev-deploy
@@ -81,9 +81,10 @@ call_avatar_create: call_avatar_create_me call_avatar_create_for_beta_tester
 call_avatar_create_me:
 	near --account_id ${NEAR_DEV_ACCOUNT} call $(shell cat neardev/dev-account) avatar_create --amount 1
 # 0.02401721 - 0.0016 = 0.02241721
-call_avatar_create_for_beta_tester:
+call_avatar_create_for_beta_tester: call_avatar_create_me
 	near --account_id ${NEAR_DEV_ACCOUNT} call $(shell cat neardev/dev-account) avatar_create_for '{"owner_id":"tb.testnet"}' --amount 1
 	near --account_id ${NEAR_DEV_ACCOUNT} call $(shell cat neardev/dev-account) avatar_create_for '{"owner_id":"jondou42.testnet"}' --amount 1
-
-#	near --account_id ${NEAR_DEV_ACCOUNT} call $(cat neardev/dev-account) nft_transfer '{ "token_id": "0", "receiver_id": "dev-1630186207494-62680183246154"}' --amount 0.000000000000000000000001
-#	near --account_id $(cat neardev/dev-account) call $(cat neardev/dev-account) nft_transfer '{ "token_id": "0", "receiver_id": "ilyar.testnet"}' --amount 0.000000000000000000000001
+demo:
+	cd demo && yarn dev:build:web && cd dist && git add . && git commit --amend -m init && git pub -f
+#	near --account_id ${NEAR_DEV_ACCOUNT} call $(cat neardev/dev-account) nft_transfer '{ "token_id": "bafkreieyck4x2tujwtvmdu4dltjmff67khqviaewzixidj5zoa2sjrc62y", "receiver_id": "dev-1630244685532-93937831175122"}' --amount 0.000000000000000000000001
+#	near --account_id $(cat neardev/dev-account) call $(cat neardev/dev-account) nft_transfer '{ "token_id": "bafkreieyck4x2tujwtvmdu4dltjmff67khqviaewzixidj5zoa2sjrc62y", "receiver_id": "ilyar.testnet"}' --amount 0.000000000000000000000001
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod identicon;
 
+use cid::multihash::{Code, MultihashDigest};
+use cid::Cid;
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
@@ -20,7 +22,7 @@ pub struct Contract {
     token: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
 }
-
+const RAW: u64 = 0x55;
 const ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -87,9 +89,9 @@ impl Contract {
     pub fn avatar_create_for(&mut self, owner_id: ValidAccountId) -> Token {
         let svg = identicon::make(&hash_account_id(&owner_id.clone().into()));
         let media = format!("svg+xml;base64,{}", base64::encode(svg.clone()));
-        let hash = env::sha256(&svg.as_bytes());
-        let media_hash = Base64VecU8(hash.clone());
-        let token_id = hex::encode(&hash);
+        let hash = Code::Sha2_256.digest(svg.as_bytes());
+        let media_hash = Base64VecU8(env::sha256(&svg.as_bytes()));
+        let token_id = Cid::new_v1(RAW, hash).to_string();
         self.token.owner_id = env::signer_account_id(); // FIXME
         self.token.mint(
             token_id,
@@ -171,8 +173,7 @@ mod tests {
             .signer_account_id(accounts(0))
             .build());
 
-        let token_id =
-            "3d8e371881abfce0eaea88596e137047a63d6d3efb91d6111f12806816d35585".to_string();
+        let token_id = "bafkreib5ry3rranl7tqov2uilfxbg4chuy6w2px3shlbchysqbubnu2vqu".to_string();
         let token = contract.avatar_create();
         assert_eq!(token.token_id, token_id);
         assert_eq!(token.owner_id, accounts(0).to_string());
@@ -193,8 +194,7 @@ mod tests {
             .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
-        let token_id =
-            "3d8e371881abfce0eaea88596e137047a63d6d3efb91d6111f12806816d35585".to_string();
+        let token_id = "bafkreib5ry3rranl7tqov2uilfxbg4chuy6w2px3shlbchysqbubnu2vqu".to_string();
         contract.avatar_create();
 
         testing_env!(context
@@ -231,8 +231,7 @@ mod tests {
             .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
-        let token_id =
-            "3d8e371881abfce0eaea88596e137047a63d6d3efb91d6111f12806816d35585".to_string();
+        let token_id = "bafkreib5ry3rranl7tqov2uilfxbg4chuy6w2px3shlbchysqbubnu2vqu".to_string();
         contract.avatar_create();
 
         // alice approves bob
@@ -263,8 +262,7 @@ mod tests {
             .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
-        let token_id =
-            "3d8e371881abfce0eaea88596e137047a63d6d3efb91d6111f12806816d35585".to_string();
+        let token_id = "bafkreib5ry3rranl7tqov2uilfxbg4chuy6w2px3shlbchysqbubnu2vqu".to_string();
         contract.avatar_create();
 
         // alice approves bob
@@ -302,8 +300,7 @@ mod tests {
             .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
-        let token_id =
-            "3d8e371881abfce0eaea88596e137047a63d6d3efb91d6111f12806816d35585".to_string();
+        let token_id = "bafkreib5ry3rranl7tqov2uilfxbg4chuy6w2px3shlbchysqbubnu2vqu".to_string();
         contract.avatar_create();
 
         // alice approves bob
